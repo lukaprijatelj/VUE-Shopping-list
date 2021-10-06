c3ceaa6c4242a1862358c9f3f8a57704022c3ea4
Luka <com.lukas@gmail.com>
2021-10-06 17:12:04 +0200

    Updated ts config

---
+++ client (Webpack, Vue, Typescript)/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+        <h1>Shopping list:</h1>
+
+        <Entry
+            v-for="item in items"
+            v-bind:key="item.id"
+            v-bind:modelData="item"
+        />
+        <AddEntry />
+    </div>
+</template>
+
+
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Entry from "./components/Entry.vue";
+import AddEntry from "./components/AddEntry.vue";
+
+export interface IShoppingEntry {
+    index: number;
+    message: string;
+}
+
+@Options({
+    components: {
+        Entry,
+		AddEntry
+    },
+    props: {},
+})
+export default class App extends Vue {
+    public items: Array<IShoppingEntry> = [];
+
+    /**
+     * DON'T USE CONSTRUCTOR. Instead use created() method, because it is part of VueJS lifecycle.
+     */
+    public created() {
+        fetch("/api")
+            .then((res) => res.json())
+            .then((data) => {
+                this.items = data.shoppingList.map(
+                    (entry: string, index: number) => ({
+                        message: entry,
+                        index: index,
+                    })
+                );
+            });
+    }
+
+    // Lifecycle hooks
+    public mounted() {}
+    public updated() {}
+    public destroyed() {}
+}
+</script>
+
+
+
+<style>
+body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+        "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+        "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+}
+</style>
